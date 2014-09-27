f38cdf7aea2d89855cdd14fe9a7034ae2a434b1e
Patrik Keinonen <pate@leka.local>
2014-09-27 18:49:38 +0300

    paskaa

--- server.js
+++ server.js
@@ -33,11 +33,11 @@
 	});*/
 
 	app.get('/api/restaurants/:lat/:lng/', function(req, res) {
-
+		restaurants = [];
 		var lng = parseFloat(req.params.lng);
 		var lat = parseFloat(req.params.lat);
 		console.log(lat);
-		var maxDelta = 0.01;
+		var maxDelta = 0.003;
 		var clientId = '5BA839F112CE4CCEBF57A49D98D29A44';
 		
 		console.log('long: ' + lng + " lat: " + lat);
@@ -46,37 +46,53 @@
 
 		client = new Client();
 
-
+		
 
 		client.get('http://www.lolnas.fi/api/restaurants.json', 'GET', function (data, response) {
-			var restaurants = [];
-			var asd = JSON.parse(data);
-
 			
+
+			var asd = JSON.parse(data);
 			_.each(asd.restaurants, function(item){
+
 				var newLng = parseFloat(item.longitude);
 				var newLat = parseFloat(item.latitude);
 				var lngDelta = Math.abs(newLng - lng);
 				var latDelta = Math.abs(newLat - lat);
 
 				if(lngDelta < maxDelta && latDelta < maxDelta){
-					var reqStr = "http://api.bing.net/json.aspx?"
-							+ "AppId=" + clientId
-							+ "&Query=" + item.name
-							+ "&Sources=Image";
+					console.log(item);
+					var reqStr = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDggXGnF2rW6E56Jv8wh12P-UnUhJJCGXQ&cx=010857121049557165748:ox6rkvzbdfs&q=" + item.name;
 
-					/*client.get(reqStr, 'GET', function (data, response){
-						console.log(data);
-					});*/
-					item.image = "http://upload.wikimedia.org/wikipedia/commons/a/a0/Lasipalatsi.jpg";
-					restaurants.push(item);				
-				}
-				
+					client.get(reqStr, 'GET', function (data, response){
+						item.image = JSON.parse(data).items[0].pagemap.cse_image[0].src;
+						console.log(item);
+
+						
+					});
+					
+					var from = lng + ',' + lat;
+					var tooo = item.longitude+ ',' + item.latitude;
+					 var dosaStr = "http://api.reittiopas.fi/hsl/prod/?request=route&user=tapioku&epsg_in=4326&epsg_out=4326&pass=htmltaulukkomies&format=json&transport_types=walk&from="+from+"&to=" + tooo;
+ 				
+ 					client.get(dosaStr, 'GET', function (data, response){
+							var jiison = JSON.parse(data);
+							item.duration = jiison[0][0].duration;
+							item.distans = jiison[0][0].length;
+							
+						
+						});	
+ 					restaurants.push(item);
+
+				}	
+
 			});
+ setTimeout(function(){
+					console.log("jou");
+								res.json(restaurants);
+				}, 2000);
 
-			res.json(restaurants);
 		});
-
+ 		
 		//res.json(stuff);
 	}); 
 
